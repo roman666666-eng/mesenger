8e81312d7c227f524ae0bd8d49d4c9b298844662
roman666666-eng <roomkis2008@gmail.com>
2025-12-16 11:57:58 +0200

    test

--- mesenger.bat
+++ mesenger.bat
@@ -1,2 +1,15 @@
 @echo off
+SETLOCAL
+
+SET "StatusFile=setings.txt"
+SET "InstallFlag="
+FOR /F "tokens=2" %%A IN (%StatusFile%) DO (
+    SET "InstallFlag=%%A"
+    GOTO CheckFlag
+)
+:CheckFlag
+IF "%InstallFlag%" EQU "0" (
+    %CD%\.venv\Scripts\python.exe mesenger_install.py
+)
+ENDLOCAL
  %CD%\.venv\Scripts\python.exe mesenger_client.py
